cab4f4f5b68e7d7b36e0ec1fdb91a9903b2e7cf1
nixholas <nicholas@slyck.net>
2017-09-19 12:12:04 +0800

    Source: Restructuring for dynamic tickers and endpoints
    Signed-off-by: nixholas <nicholas@slyck.net>

--- MMM-Stock.js
+++ MMM-Stock.js
@@ -6,12 +6,13 @@ Module.register("MMM-Stock", {
   defaults: {
     updateInterval: 60000,
     fadeSpeed: 1000,
-    tickers: ['BTCUSD', 'YHOO'],
     currency: 'usd',
-    baseURL: 'https://coinhako.com/api/v1/price/currency/',
-    baseURLBFX: 'https://api.bitfinex.com/v1',
-    baseURLBTRX: 'https://bittrex.com/api/v1.1/public/getticker', // Requires parameter market (market = BTC-LTC)
-    baseURLBTSTMP: 'https://www.bitstamp.net/api/ticker/'
+    baseURLs: ['https://coinhako.com/api/v1/price/currency/', 
+                'https://api.bitfinex.com/v1', 
+                'https://bittrex.com/api/v1.1/public/getticker', // Requires parameter market (market = BTC-LTC)
+                'https://www.bitstamp.net/api/ticker/'],
+    baseTickerURLs: ['https://www.coinhako.com/api/v1/wallet/supported_currencies', 
+                'https://api.bitfinex.com/v1/symbols']
   },
 
   getStyles: function() {
@@ -20,9 +21,6 @@ Module.register("MMM-Stock", {
 
   start: function() {
     this.getStocks();
-    if(this.config.currency.toLowerCase() != 'usd'){
-      this.getExchangeRate();
-    }
     this.scheduleUpdate();
   },
 
@@ -39,13 +37,6 @@ Module.register("MMM-Stock", {
     var count =  data.query.count;
     //console.log(count)
 
-    //if another currency is required - usd is default
-    // var differentCurrency = false;
-    // if(this.config.currency.toLowerCase() != 'usd'){
-    //   differentCurrency = true;
-    //   var requiredCurrency = this.config.currency.toUpperCase();
-    // }
-    
     for (var i = 0; i < count; i++) {
       var stockData = data.query.results.quote[i];
       var symbol = stockData.symbol;
@@ -67,16 +58,9 @@ Module.register("MMM-Stock", {
       }
       html = html + "<span class='" + priceClass + "'>";
       html = html + "<span class='quote'>" + name + " (" + symbol + ")</span> ";
-      //if(differentCurrency){
-        //convert between currencies
-        // var exchangeRate = this.rate.query.results.rate;
-        // if(exchangeRate.Bid && exchangeRate.Bid != "N/A"){
-        //   price = parseFloat(price) * parseFloat(exchangeRate.Bid);
-        // }
-        // html = html + parseFloat(price).toFixed(2) + " " + requiredCurrency;
-      //} else {
-        html = html + parseFloat(price).toFixed(2) + " " + stockData.Currency;
-      //}
+      
+      html = html + parseFloat(price).toFixed(2) + " " + stockData.Currency;
+        
       html = html + "<span class='" + priceIcon + "'></span>" + parseFloat(Math.abs(change)).toFixed(2) + " (";
       html = html + parseFloat( Math.abs(pChange.split('%')[0])).toFixed(2) + "%)</span>";
 
@@ -103,31 +87,18 @@ Module.register("MMM-Stock", {
     var that = this;
     setInterval(function() {
       that.getStocks();
-      // if(this.config.currency.toLowerCase() != 'usd'){
-      //   that.getExchangeRate();
-      // }
     }, loadTime);
   },
 
   getStocks: function () {
-    var url = this.config.baseURL + "?q=env%20'store%3A%2F%2Fdatatables.org%2Falltableswithkeys'%3B%20select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20('" + this.config.tickers.join(', ') + "')&format=json&diagnostics=true&callback=";
-    this.sendSocketNotification('GET_STOCKS', url);
+    this.sendSocketNotification('GET_STOCKS', baseURLs, baseTickerURLs);
   },
-
-  // getExchangeRate: function () {
-  //   var url = this.config.baseURL + "?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20('USD" + this.config.currency + "')&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-  //   this.sendSocketNotification('GET_EXCHANGE_RATE', url);
-  // },
 
   socketNotificationReceived: function(notification, payload) {
     if (notification === "STOCK_RESULT") {
       this.result = payload;
       this.updateDom(self.config.fadeSpeed);
     }
-    // else if(notification === "EXCHANGE_RATE"){
-    //   this.rate = payload;
-
-    // }
   },
 
 });
--- node_helper.js
+++ node_helper.js
@@ -6,37 +6,51 @@ module.exports = NodeHelper.create({
     console.log(this.name + ' helper method started...');
   },
 
-  sendRequest: function (url) {
+  // For retrieving ticker price
+  sendRequest: function (url, tickers) {
       var self = this;
 
-      request({ url: url, method: 'GET' }, function (error, response, body) {
+      for (i = 0; i < tickers.length; i++) {
+        request({ url: url + tickers[i], method: 'GET' }, function (error, response, body) {
           if (!error && response.statusCode == 200) {
             var result = JSON.parse(body);
             //console.log(result)
             self.sendSocketNotification('STOCK_RESULT', result);
           }
-      });
+        });
+      }
 
-  },
-
-  sendExchangeRate: function (url) {
-    var self = this;
-
-      request({ url: url, method: 'GET' }, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var result = JSON.parse(body);
-            self.sendSocketNotification('EXCHANGE_RATE', result);
-          }
-      });
   },
 
   //Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, url) {
+  socketNotificationReceived: function(notification, urls, tickers) {
     if (notification === 'GET_STOCKS') {
       //console.log(url)
-      this.sendRequest(url);
-    } else if(notification === 'GET_EXCHANGE_RATE'){
-      this.sendExchangeRate(url);
+      
+      // ========= Retrieve the tickers first ========= //
+
+      var hakoTickers = new Array();
+      // CoinHako first
+      request({
+        method: 'GET',
+        url: tickers[0]
+      })
+        .then(function (resp) {
+          var response = JSON.Parse(resp);
+          for (i = 0; i < response["data"]["crypto_currencies"]; i++) {
+            for (j = 0; j < response["data"]["fiat_currencies"]; j++) {
+                hakoTickers.push(response["data"]["crypto_currencies"][i]["symbol"] + response["data"]["fiat_currencies"][j]["symbol"]);
+            }
+          }
+      });
+  
+      // Bitfinex
+
+      // Bittrex
+
+      // Bitstamp
+
+      this.sendRequest(urls[0], hakoTickers);
     }
   }
 
