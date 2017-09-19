72e39f8d3db036bbd94374a01a7d775d2488c127
nixholas <nicholas@slyck.net>
2017-09-19 15:54:18 +0800

    node_helper: variable object resolution
    Signed-off-by: nixholas <nicholas@slyck.net>

--- node_helper.js
+++ node_helper.js
@@ -19,9 +19,9 @@ module.exports = NodeHelper.create({
             //console.log("node_helper.js: body json parsed -> " + result);
 
             for (i = 0; i < result["data"]["crypto_currencies"].length; i++) {
-              console.log("i = " + i);
+              //console.log("i = " + i);
               for (j = 0; j < result["data"]["fiat_currencies"].length; j++) {
-                  console.log("node_helper.js: hakoticker obj -> " + result["data"]["crypto_currencies"][i]);
+                  //console.log("node_helper.js: hakoticker obj -> " + result["data"]["crypto_currencies"][i]);
                   hakoTickers.push(result["data"]["crypto_currencies"][i]["symbol"] + result["data"]["fiat_currencies"][j]["symbol"]);
               }
             }
@@ -30,14 +30,15 @@ module.exports = NodeHelper.create({
             for (k = 0; k < hakoTickers.length; k++) {
               request({ url: 'https://coinhako.com/api/v1/price/currency/' + hakoTickers[k], method: 'GET' }, function (error, response, body) {
                 if (!error && (response.statusCode == 200 || response.statusCode == 429)) {
+                  console.log("node_helper.js: price api body -> " + body);
                   var result = JSON.parse(body);
                   
-                  var elementPair;
+                  var elementPair = {
+                    pair_name: hakoTickers[k],
+                    buy_price: result["data"]["buy_price"],
+                    sell_price: result["data"]["sell_price"]
+                  };
 
-                  elementPair.pair_name = hakoTickers[k];
-                  elementPair.buy_price = result["data"]["buy_price"];
-                  elementPair.sell_price = result["data"]["sell_price"];
-                  
                   hakoPairs.push(elementPair);
                 }
               });
@@ -48,9 +49,10 @@ module.exports = NodeHelper.create({
 
           if (error) {
             console.log("[ERROR] node_helper.js: Error -> " + error);
-          } else {
-            console.log("Status Code: " + response.statusCode);
-          }
+          } 
+          // else {
+          //   console.log("Status Code: " + response.statusCode);
+          // }
       });
 
   },
