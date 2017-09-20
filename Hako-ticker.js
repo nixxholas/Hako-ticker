94ae9ebe6768b9a3d17bab49cfef12d9cec29271
nixholas <nicholas@slyck.net>
2017-09-20 13:47:46 +0800

    Hako-ticker: i variable declaration
    Signed-off-by: nixholas <nicholas@slyck.net>

--- Hako-ticker.js
+++ Hako-ticker.js
@@ -44,8 +44,7 @@ Module.register("Hako-ticker", {
       console.log("getDom():" +  JSON.stringify(data));
     }
 
-    console.log(data.data.length);
-
+    var i;
     for (i = 0; i < data["data"].length; i++) {
       if (this.config.debugging) {
         console.log("getDom() data array element:" +  data["data"][i]);
