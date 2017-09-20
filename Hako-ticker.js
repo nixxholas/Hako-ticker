6b44c140c9b0b530413c4436e9f824387e79cf3b
nixholas <nicholas@slyck.net>
2017-09-20 15:32:25 +0800

    Hako-ticker: Debuggable for currData
    Signed-off-by: nixholas <nicholas@slyck.net>

--- Hako-ticker.js
+++ Hako-ticker.js
@@ -45,7 +45,11 @@ Module.register("Hako-ticker", {
     var wrapper = document.createElement("ticker");
     wrapper.className = 'medium bright';
     wrapper.className = 'marquee ticker';
-    currData = this.currData;
+    currentData = this.currData;
+
+    if (this.config.debugging) {
+      console.log("getDom():" + currData);
+    }
 
     var data = this.result;
 
@@ -53,15 +57,15 @@ Module.register("Hako-ticker", {
     //   console.log("getDom():" +  JSON.stringify(data));
     // }
 
-    if (currData) { // If current data exists
+    if (currentData) { // If current data exists
       // We iterate through each of them and do a cross check.
       console.log("getDom() pushing all pairs with checks");
       for (var pair in data.data) {
         for (i = 0; i < currData.length; i++) {
           // TODO: PERFORM NOT FOUND CHECKS
-          if (currData[i].getName() == pair.getName()) {
-            currData[i].setBuyPrice(data.data[pair]["buy_price"]);
-            currData[i].setSellPrice(data.data[pair]["sell_price"]);
+          if (currentData[i].getName() == pair.getName()) {
+            currentData[i].setBuyPrice(data.data[pair]["buy_price"]);
+            currentData[i].setSellPrice(data.data[pair]["sell_price"]);
             break;
           }
         }
@@ -79,13 +83,13 @@ Module.register("Hako-ticker", {
         //   console.log("getDom() pair object:" +  data.data[pair]);
         // }
         
-        var pair = new Pair(pair, data.data[pair]["buy_price"], data.data[pair]["sell_price"]);
+        let pair = new Pair(pair, data.data[pair]["buy_price"], data.data[pair]["sell_price"]);
         
         if (this.config.debugging) {
           console.log("getDom() pair object:" +  pair.getName());
         }
         
-        currData.push(pair);
+        currentData.push(pair);
       }
     }
 
